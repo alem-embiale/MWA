325827ed72ed79b25253eba034e037b0ee6a8fe7
Alem Embiale <alemwatch@gmail.com>
2018-10-04 23:00:28 -0500

    lesson2

--- L2E2.js
+++ L2E2.js
@@ -1,42 +1,16 @@
-const dns = require('dns');
-const { from } = require('rxjs');
+Array.prototype.even = function(){
+	setTimeout(() => {
+		console.log(this.filter((item) => item%2 == 0));
+	},0);
+};
 
-const hostname = "www.mum.edu"; 
+Array.prototype.odd = function(){
+	setTimeout(() => {
+		console.log(this.filter((item) => item%2 > 0));
+	},0);
+};
 
-// direct
-dns.resolve4(hostname,(err, addresses) => {
-	if(err){
-		console.log("Direct:", err.message);
-	}else{
-		console.log("Direct:", addresses);
-	}
-});
-
-// using promise
-const ipPromise = (url) => new Promise((resolve, reject) => {
-	dns.resolve4(url,(err, addresses) => {
-		if(err){
-			reject(err.message);
-		}else{
-			resolve(addresses);
-		}
-	});
-});
-
-ipPromise(hostname)
-	.then((data) => console.log("Promise:", data))
-	.catch((err) => console.log("Promise:", err));
-
-// using Async/wait
-async function getIPAsync(url){
-	try{
-		const ip = await ipPromise(url);
-		console.log("AsyncWait:", ip);
-	}catch(err){
-		console.log("AsyncWait:", err);
-	}
-}
-getIPAsync(hostname);
-
-// using observables
-from(ipPromise(hostname)).subscribe(info => console.log("Observable:",info));
+console.log('start');
+[1,2,3,4,5,6,7,8].even();
+[1,2,3,4,5,6,7,8].odd();
+console.log('end');
